b3adc7377e51fd16f36641d78832b7444b90b326
Dakota Whitney <whitneydakota@gmail.com>
2022-03-30 15:30:02 -0400

    changed autofillAppleStore() to anonymous func

--- content_scripts/apple-store.js
+++ content_scripts/apple-store.js
@@ -1,5 +1,5 @@
 (async () => {
-    const autofillAppleStore = marketDataObject => {
+    const autofillAppleStore = () => {
         const selectorMetadataMap = {
             "div[name='promotionalText']": "Promotional Text",
             "div[name='whatsNew']": "What's New  in This Version",
@@ -17,7 +17,7 @@
         console.log(marketData);
         for(let [inputElement,metadataKey] of Object.entries(selectorMetadataMap)){
             inputElement = document.querySelector(inputElement);
-            const {[metadataKey]:metadata} = marketDataObject;
+            const {[metadataKey]:metadata} = marketData;
             if(inputElement && !inputElement.disabled && metadata){
                 inputElement.dispatchEvent(new InputEvent('input'));
                 inputElement.value = metadata;
@@ -28,13 +28,13 @@
         };
         document.querySelector("button.select-builds-button___1E97t") ? document.querySelector("button.select-builds-button___1E97t").click() : undefined;
         console.log('App Store Connect URLs:');
-        for(const [storeId,storeData] of Object.entries(marketDataObject)) console.log(`${storeData.market}\n${storeData.storeUrl}`);
+        for(const [storeId,storeData] of Object.entries(marketData)) console.log(`${storeData.market}\n${storeData.storeUrl}`);
         console.groupEnd();
     };
     const handleMutation = mutationList => {
         mutationList.forEach(mutationRecord => {
-            const {["Version"]:newVersion} = marketData;
             if(mutationRecord.target.querySelector("div.ReactVirtualized__Table__row.selectable")){
+                const {["Version"]:newVersion} = marketData;
                 const versionBuild = Array.from(document.querySelectorAll("div.ReactVirtualized__Table__row.selectable")).find(build => build.children[2].innerText == newVersion);
                 if(versionBuild) versionBuild.click();
                 const doneButton = Array.from(document.querySelectorAll("div.ReactModalPortal")).find(modal => modal.innerText.includes("Add Build")).querySelector("button[type='primary']");
@@ -43,7 +43,7 @@
                     observer.disconnect();
                 };
             };
-            if(mutationRecord.target.querySelector("input[name='versionString'].has-meta")) autofillAppleStore(marketData);
+            if(mutationRecord.target.querySelector("input[name='versionString'].has-meta")) autofillAppleStore();
         });
     };
     const appleId = window.location.href.split("/").find(path => path.match(/^\d+$/));
@@ -51,5 +51,5 @@
     const {[appleId]:marketData} = await chrome.storage.local.get(appleId);
     const observer = new MutationObserver(handleMutation);
     observer.observe(document.querySelector("body"),{childList:true,subtree:true});
-    chrome.runtime.onMessage.addListener((message,sender,sendResponse) => autofillAppleStore(marketData))
+    chrome.runtime.onMessage.addListener((message,sender,sendResponse) => autofillAppleStore())
 })();
